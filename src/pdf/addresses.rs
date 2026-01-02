f680b708881203b6140bb33cc5f541cf344100c5
Julian Tepper <Juliantepper2006+github@gmail.com>
2026-01-02 22:44:24 +0100

    removed draw_line at bottom due to product table changes

--- src/pdf/addresses.rs
+++ src/pdf/addresses.rs
@@ -7,7 +7,6 @@ pub fn draw_address_section(ctx: &mut PdfContext, invoice: &Invoice) {
     ctx.write_text_at("SOLD BY:", 8.0, COL_1, addr_y);
     ctx.write_text_at("BILLED TO:", 8.0, COL_2, addr_y);
 
-    // Seller Column
     ctx.y = addr_y - Mm(5.0);
     ctx.write_text(&invoice.seller.name, 10.0, COL_1);
     for line in invoice.seller.address.lines() {
@@ -16,7 +15,6 @@ pub fn draw_address_section(ctx: &mut PdfContext, invoice: &Invoice) {
     ctx.write_text(&format!("VAT: {}", invoice.seller.vat_id), 9.0, COL_1);
     let seller_end_y = ctx.y;
 
-    // Buyer Column
     ctx.y = addr_y - Mm(5.0);
     ctx.write_text(&invoice.buyer.name, 10.0, COL_2);
     for line in invoice.buyer.address.lines() {
@@ -25,6 +23,5 @@ pub fn draw_address_section(ctx: &mut PdfContext, invoice: &Invoice) {
     ctx.write_text(&invoice.buyer.email, 9.0, COL_2);
     let buyer_end_y = ctx.y;
 
-    ctx.y = seller_end_y.min(buyer_end_y) - Mm(10.0);
-    draw_line(&mut ctx.current_ops, COL_1, Mm(190.0), ctx.y);
+    ctx.y = seller_end_y.min(buyer_end_y);
 }
